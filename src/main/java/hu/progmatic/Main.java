00a83c1aeb1eeaac82eb9cd9b53688dd846832cd
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-05-02 18:13:46 +0200

    bm: 3. + 4. feladat

---
+++ src/main/java/hu/progmatic/Main.java
@@ -0,0 +1,14 @@
+package hu.progmatic;
+
+import hu.progmatic.model.City;
+import hu.progmatic.model.TrainTravel;
+import hu.progmatic.model.Travel;
+
+public class Main {
+    public static void main(String[] args) {
+        City budapest = new City("CITY001", "Budapest", "HU", 47.50, 19.04);
+        City gyor = new City("CITY002", "Győr", "HU", 47.68, 17.65);
+        Travel travel = new TrainTravel(budapest, gyor);
+        System.out.println(travel);
+    }
+}
