c7e6f4d67be685a2ba8a5fc983f7711d95285642
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-05-02 18:31:24 +0200

    bm: 3. + 4. feladat

--- src/main/java/hu/progmatic/Main.java
+++ src/main/java/hu/progmatic/Main.java
@@ -1,6 +1,7 @@
 package hu.progmatic;
 
 import hu.progmatic.model.City;
+import hu.progmatic.model.PlaneTravel;
 import hu.progmatic.model.TrainTravel;
 import hu.progmatic.model.Travel;
 
@@ -8,7 +9,9 @@ public class Main {
     public static void main(String[] args) {
         City budapest = new City("CITY001", "Budapest", "HU", 47.50, 19.04);
         City gyor = new City("CITY002", "Győr", "HU", 47.68, 17.65);
-        Travel travel = new TrainTravel(budapest, gyor);
-        System.out.println(travel);
+        Travel trainTravel = new TrainTravel(budapest, gyor);
+        System.out.println(trainTravel);
+        Travel planeTravel = new PlaneTravel(budapest, gyor);
+        System.out.println(planeTravel);
     }
 }
---
+++ src/main/java/hu/progmatic/model/PlaneTravel.java
@@ -0,0 +1,31 @@
+package hu.progmatic.model;
+
+public final class PlaneTravel extends Travel {
+    public PlaneTravel() {
+    }
+
+    public PlaneTravel(City cityFrom, City cityTo) {
+        super(cityFrom, cityTo);
+    }
+
+    @Override
+    public double getTime() {
+        return 2.0 + getDistance() / 800.0;
+    }
+
+    @Override
+    public double getPrice() {
+        if (getDistance() <= 1000.0) {
+            return 20_000.0;
+        } else if (getDistance() <= 2500.0) {
+            return 50_000.0;
+        } else {
+            return 100_000.0;
+        }
+    }
+
+    @Override
+    public String getVehicle() {
+        return "Plane";
+    }
+}
