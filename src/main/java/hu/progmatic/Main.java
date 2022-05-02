74e8fd9b8b99e0981adcd8cdce9e647c8d26ff3a
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-05-02 19:13:42 +0200

    bm: 5. feladat

--- src/main/java/hu/progmatic/Main.java
+++ src/main/java/hu/progmatic/Main.java
@@ -1,17 +1,22 @@
 package hu.progmatic;
 
-import hu.progmatic.model.City;
-import hu.progmatic.model.PlaneTravel;
-import hu.progmatic.model.TrainTravel;
-import hu.progmatic.model.Travel;
+import hu.progmatic.model.*;
 
 public class Main {
     public static void main(String[] args) {
         City budapest = new City("CITY001", "Budapest", "HU", 47.50, 19.04);
         City gyor = new City("CITY002", "Győr", "HU", 47.68, 17.65);
-        Travel trainTravel = new TrainTravel(budapest, gyor);
-        System.out.println(trainTravel);
-        Travel planeTravel = new PlaneTravel(budapest, gyor);
-        System.out.println(planeTravel);
+        Travel[] travels = {
+                new TrainTravel(budapest, gyor),
+                new PlaneTravel(budapest, gyor)
+        };
+        Activity[] activities = {
+                new Activity("sétálás", 1.0),
+                new Activity("mozizás", 2.5)
+        };
+        Product[] products = {
+                new Product("mogyoró", 500.0),
+                new Product("sör", 169.0)
+        };
     }
 }
