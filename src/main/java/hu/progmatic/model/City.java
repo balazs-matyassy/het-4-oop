5582ff340af0877504697465b2b85825c0abf659
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-05-02 17:24:04 +0200

    bm: 1. feladat

--- src/main/java/hu/progmatic/model/City.java
+++ src/main/java/hu/progmatic/model/City.java
@@ -1,5 +1,83 @@
 package hu.progmatic.model;
 
-public class City {
+import java.util.Objects;
 
+public class City {
+    private final String id;
+    private String name;
+    private String countryCode;
+    private double latitude;
+    private double longitude;
+
+    public City(String id) {
+        this.id = id;
+    }
+
+    public City(String id, String name, String countryCode, double latitude, double longitude) {
+        this.id = id;
+        this.name = name;
+        this.countryCode = countryCode;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return id.equals(city.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", countryCode='" + countryCode + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
 }
